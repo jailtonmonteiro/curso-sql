1e5b588f243f4338348a25f97c440ba762670562
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-11-08 05:02:13 -0300

    Novas aulas.

--- Exercicios/04_where.sql
+++ Exercicios/04_where.sql
@@ -1,7 +1,8 @@
 -- Lista de clientes com 100 a 200 pontos (inclusive ambos)
 
-SELECT idCliente,
+SELECT  idCliente,
         qtdePontos
 FROM clientes
-WHERE qtdePontos >= 100
-AND QtdePontos <= 200
+-- WHERE qtdePontos >= 100
+-- AND QtdePontos <= 200
+WHERE qtdePontos BETWEEN 100 AND 200
