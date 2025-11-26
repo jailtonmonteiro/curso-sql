2863f92bd1725ea6b06debf108d7e6cc7225fe30
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-11-25 23:02:28 -0300

    Atualizando o repositório

--- 11_stats.sql
+++ 11_stats.sql
@@ -1,5 +1,6 @@
 SELECT  round(avg(qtdePontos),2) AS MediaPontos,
         min(qtdePontos) AS MinCarteira,
         max(qtdePontos) AS MaxCarteira,
-        sum(flTwitch) AS QtdTwitch
+        sum(flTwitch) AS QtdTwitch,
+        sum(flEmail) AS QtdEmail
 FROM clientes
